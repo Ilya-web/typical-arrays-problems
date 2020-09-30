50af15a233bcf899c46a7777edff0177639e5aaa
FrontTestSite <testsitewinc@gmail.com>
2020-09-30 11:22:12 +0300

    feat: finish work project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+
+    if(array && array.length > 0) {
+        return Math.min.apply(Math, array);
+    }
+      return 0;
+
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+
+    if(array && array.length > 0) {
+        return Math.max.apply(Math, array);
+    }
+    return 0;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let result = 0;
+    if(array && array.length > 0) {
+        for(let i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+        return result / array.length;
+    }
+
+    return 0;
+
+};
